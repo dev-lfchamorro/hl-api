9238d4a60921b2143e97d39ee4dcaba3a55b40e8
Luis Fernando <dev.lfchamorro@gmail.com>
2024-04-12 19:27:34 -0500

    Set header for json responses and receive the method type of request

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
-    $host = "localhost";
-    $user = "root";
-    $pass = "root";
-    $db_name = "hl_api";
+$host = "localhost";
+$user = "root";
+$pass = "root";
+$db_name = "hl_api";
 
-    $conn = new mysqli($host, $user, $pass, $db_name);
+$conn = new mysqli($host, $user, $pass, $db_name);
 
-    if ($conn->connect_error) {
-        die("Connection failed: ". mysqli_connect_error());
-    }
-?>
+// Testing connection
+if ($conn->connect_error) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Setting headers and receiving verbs
+header('Content-Type: application/json');
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+print_r($method);
