c981d97cfc40cc025214a46603408ab5563549ac
Luis Fernando <dev.lfchamorro@gmail.com>
2024-04-15 20:57:12 -0500

    Contacts logic for actions URL param was created

---
+++ api/v1/contacts/index.php
@@ -0,0 +1,22 @@
+<?php
+
+header('Content-Type: application/json');
+
+$request_uri = $_SERVER['REQUEST_URI'];
+$api_version = 'v1';
+
+$api_request = str_replace("/hl-api/api/$api_version/contacts/", '', $request_uri);
+$api_parts = explode('/', $api_request);
+
+$action = isset($api_parts[0]) ? $api_parts[0] : null;
+
+switch ($action) {
+    case 'get-contacts':
+        echo json_encode($action);
+        break;
+
+    default:
+        http_response_code(404);
+        echo json_encode(array("status" => "error", "message" => "Invalid action"));
+        break;
+}
