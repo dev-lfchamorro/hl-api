91fe98b179d04a7bfa19e5262ae3f6d0bbe2c01b
Luis Fernando <dev.lfchamorro@gmail.com>
2024-04-15 23:02:29 -0500

    Helper function to parse api-request was refactored

--- helpers/parse-api-request.php
+++ helpers/parse-api-request.php
@@ -1,8 +1,10 @@
 <?php
-function parseApiRequest($api_version, $segment = null)
+function parseApiRequest($segment = null)
 {
+    require_once '../../config/config.php';
+
     $request_uri = $_SERVER['REQUEST_URI'];
-    $api_request = str_replace("/hl-api/api/$api_version/$segment", '', $request_uri);
+    $api_request = str_replace("/hl-api/api/" . API_VERSION . "/$segment", '', $request_uri);
     $api_parts = explode('/', $api_request);
 
     $ulr_resource = isset($api_parts[0]) ? $api_parts[0] : null;
