909b33e6771f276f6e50575ac092242017c6c1a8
Luis Fernando <dev.lfchamorro@gmail.com>
2024-04-21 15:23:53 -0500

    Correct path validation was added

--- api/v1/contacts/index.php
+++ api/v1/contacts/index.php
@@ -1,6 +1,13 @@
 <?php
 require_once dirname(__FILE__) . '../../../../helpers/parse-api-request.php';
 
+$request_uri = $_SERVER['REQUEST_URI'];
+
+if (basename($request_uri) === 'contacts') {
+    http_response_code(400);
+    die(json_encode(array("status" => "error", "message" => "Bad request, invalid action")));
+}
+
 $action = parseApiRequest('contacts/');
 
 switch ($action) {
