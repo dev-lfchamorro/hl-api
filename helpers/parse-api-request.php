97f43f8db27ca5776ff87dbe5b30a1a830aad278
Luis Fernando <dev.lfchamorro@gmail.com>
2024-08-11 16:17:08 -0500

    Implementing new config variable for environment path

--- helpers/parse-api-request.php
+++ helpers/parse-api-request.php
@@ -4,7 +4,7 @@ function parseApiRequest($segment = null)
     require_once '../../config/config.php';
 
     $request_uri = $_SERVER['REQUEST_URI'];
-    $api_request = str_replace("/hl-api/api/" . API_VERSION . "/$segment", '', $request_uri);
+    $api_request = str_replace(API_PATH . "/api/" . API_VERSION . "/$segment", '', $request_uri);
     $api_parts = explode('/', $api_request);
 
     $ulr_resource = isset($api_parts[0]) ? $api_parts[0] : null;
