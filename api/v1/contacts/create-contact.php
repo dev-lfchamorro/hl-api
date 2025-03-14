ee1d7b28bde27cadbc2a5c9d2b388a978e10d75f
Luis Fernando <dev.lfchamorro@gmail.com>
2025-03-13 21:30:51 -0500

    Headers added to prevent response errors from Frontend

--- api/v1/contacts/create-contact.php
+++ api/v1/contacts/create-contact.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
 header('Content-Type: application/json');
 
 require_once dirname(__FILE__) . '../../../../config/database.php';
